8db57644407b0491c4acfccb0efecb07af2890bf
dafaath <dafa.athaullah@bytedance.com>
2024-12-26 17:21:03 +0700

    feat(work-order): Add sticky action button on form

--- web/src/admin/layout.tsx
+++ web/src/admin/layout.tsx
@@ -1,8 +1,31 @@
-import type {ReactNode} from 'react';
-import {CheckForApplicationUpdate, Layout} from 'react-admin';
+import Box from '@mui/material/Box';
+import { type ReactNode } from 'react';
+import { AppBar, CheckForApplicationUpdate, Layout, TitlePortal } from 'react-admin';
+import { useLocation } from "react-router-dom";
+
+const MyAppBar = () => {
+    const location = useLocation()
+    const appBarAlwaysOn = () => {
+        if (location.pathname.includes('/work-order/')) {
+            return true;
+        }
+
+        return false
+    }
+
+    return (
+        <AppBar color="primary" sx={{
+            position: 'fixed',
+        }} alwaysOn={appBarAlwaysOn()}>
+            <TitlePortal />
+            <Box flex="1" />
+        </AppBar>
+    )
+};
+
 
 export const DefaultLayout = ({ children }: { children: ReactNode }) => (
-    <Layout sx={{}}>
+    <Layout sx={{}} appBar={MyAppBar}>
         {children}
         <CheckForApplicationUpdate />
     </Layout>
