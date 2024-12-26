82367d691404adf6eb5306d023e82c1e0b9c1ce5
Andika Nugraha <andika.nugraha@bytedance.com>
2024-12-26 12:55:13 +0700

    feat(patient): fix patient pagination

--- internal/entity/observation.go
+++ internal/entity/observation.go
@@ -4,11 +4,11 @@ import "time"
 
 type ObservationRequest struct {
 	ID              int64     `json:"id" gorm:"primaryKey;autoIncrement"`
-	TestCode        string    `json:"test_code" gorm:"not null;index:observation_request_uniq,unique,priority:2" validate:"required,observation-type"`
+	TestCode        string    `json:"test_code"`
 	TestDescription string    `json:"test_description"`
 	RequestedDate   time.Time `json:"requested_date"`
 	ResultStatus    string    `json:"result_status"`
-	SpecimenID      int64     `json:"specimen_id" gorm:"not null;index:observation_request_uniq,unique,priority:1" validate:"required"`
+	SpecimenID      int64     `json:"specimen_id"`
 	CreatedAt       time.Time `json:"created_at" gorm:"not null"`
 	UpdatedAt       time.Time `json:"updated_at" gorm:"not null"`
 }
