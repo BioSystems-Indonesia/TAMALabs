40c7f1311799d048640cef10f0a4374cc82b1a1a
egon12 <egon.firman@gmail.com>
2025-01-07 19:29:28 +0700

    adding open browser

--- cmd/rest/main.go
+++ cmd/rest/main.go
@@ -1,6 +1,12 @@
 package main
 
 import (
+	"fmt"
+	"log"
+	"os/exec"
+	"runtime"
+	"time"
+
 	"github.com/oibacidem/lims-hl-seven/config"
 	"github.com/oibacidem/lims-hl-seven/internal/app"
 )
@@ -12,6 +18,7 @@ func main() {
 		panic(err)
 	}
 	server := app.InitRestApp(&cfg)
+	go openb()
 	server.Serve()
 }
 
@@ -22,4 +29,28 @@ func main_tcp() {
 	}
 	server := app.InitTCPApp(&cfg)
 	server.Serve()
+}
+
+func openb() {
+	time.Sleep(3 * time.Second)
+	openbrowser("http://127.0.0.1:8322")
+}
+
+
+func openbrowser(url string) {
+	var err error
+
+	switch runtime.GOOS {
+	case "linux":
+		err = exec.Command("xdg-open", url).Start()
+	case "windows":
+		err = exec.Command("rundll32", "url.dll,FileProtocolHandler", url).Start()
+	case "darwin":
+		err = exec.Command("open", url).Start()
+	default:
+		err = fmt.Errorf("unsupported platform")
+	}
+	if err != nil {
+		log.Fatal(err)
+	}
 }
