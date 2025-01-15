8b01cf101fe529c7aa9725389b0681d0deb21057
egon12 <egon.firman@gmail.com>
2025-01-15 19:51:25 +0700

    adding to store specimen

--- internal/usecase/analyzer/main.go
+++ internal/usecase/analyzer/main.go
@@ -2,21 +2,25 @@ package analyzer
 
 import (
 	"github.com/oibacidem/lims-hl-seven/internal/repository"
+	"github.com/oibacidem/lims-hl-seven/internal/repository/sql/specimen"
 )
 
 // Usecase is a struct handle HLSeven
 type Usecase struct {
 	ObservationResultRepository  repository.ObservationResult
 	ObservationRequestRepository repository.ObservationRequest
+	SpecimenRepository           *specimen.Repository
 }
 
 // NewUsecase returns a new HLSeven
 func NewUsecase(
 	observationResultRepository repository.ObservationResult,
 	observationRequestRepository repository.ObservationRequest,
+	specimenRepository *specimen.Repository,
 ) *Usecase {
 	return &Usecase{
 		ObservationResultRepository:  observationResultRepository,
 		ObservationRequestRepository: observationRequestRepository,
+		SpecimenRepository:           specimenRepository,
 	}
 }
--- internal/usecase/analyzer/observation.go
+++ internal/usecase/analyzer/observation.go
@@ -2,12 +2,25 @@ package analyzer
 
 import (
 	"context"
+	"log"
+
 	"github.com/oibacidem/lims-hl-seven/internal/entity"
 )
 
 func (u *Usecase) ProcessOULR22(ctx context.Context, data entity.OUL_R22) error {
 	specimens := data.Specimens
 	for i := range specimens {
+		spEntities, err := u.SpecimenRepository.FindByBarcode(ctx, specimens[i].HL7ID)
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+		var spEntity entity.Specimen
+
+		if len(spEntities) > 0 {
+			spEntity = spEntities[0]
+		}
+
 		//if specimens[i].ObservationRequest != nil {
 		//	err := u.ObservationRequestRepository.CreateMany(ctx, specimens[i].ObservationRequest)
 		//	if err != nil {
@@ -16,6 +29,9 @@ func (u *Usecase) ProcessOULR22(ctx context.Context, data entity.OUL_R22) error 
 		//}
 		//
 		if specimens[i].ObservationResult != nil {
+			for j := range specimens[i].ObservationResult {
+				specimens[i].ObservationResult[j].SpecimenID = int64(spEntity.ID)
+			}
 			err := u.ObservationResultRepository.CreateMany(ctx, specimens[i].ObservationResult)
 			if err != nil {
 				return err
