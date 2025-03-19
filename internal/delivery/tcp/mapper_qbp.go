64a8525fab2ada99367e70d573dead4c60deff78
Andika Nugraha <m.andika.nugraha@gmail.com>
2025-03-20 01:02:02 +0700

    feat(QPB): decode qpb

---
+++ internal/delivery/tcp/mapper_qbp.go
@@ -0,0 +1,14 @@
+package tcp
+
+import (
+	"github.com/kardianos/hl7/h251"
+	"github.com/oibacidem/lims-hl-seven/internal/entity"
+)
+
+func MapQBPQ11ToEntity(msg *h251.QBP_Q11) (entity.QBP_Q11, error) {
+	msh := mapMSHToEntity(msg.MSH)
+
+	return entity.QBP_Q11{
+		Msh: msh,
+	}, nil
+}
