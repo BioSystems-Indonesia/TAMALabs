1aeb98a054ece9a09c3cbfee619d222eca7cb562
Andika Nugraha <andika.nugraha@bytedance.com>
2025-05-13 16:16:43 +0700

    feat(a15): read file from server

--- internal/usecase/analyzer/a15.go
+++ internal/usecase/analyzer/a15.go
@@ -4,9 +4,8 @@ import (
 	"context"
 	"encoding/csv"
 	"fmt"
-	"log/slog"
+	"io"
 	"net"
-	"slices"
 	"strconv"
 	"strings"
 	"time"
@@ -17,12 +16,12 @@ import (
 
 // A15Result represents a single lab result entry.
 type A15Result struct {
-	PatientName string    `json:"patient_name"`
-	TestName    string    `json:"test_name"`
-	SampleType  string    `json:"sample_type"`
-	Value       float64   `json:"value"`
-	Unit        string    `json:"unit"`
-	Timestamp   time.Time `json:"timestamp"`
+	PatientID  string    `json:"patient_id"`
+	TestName   string    `json:"test_name"`
+	SampleType string    `json:"sample_type"`
+	Value      float64   `json:"value"`
+	Unit       string    `json:"unit"`
+	Timestamp  time.Time `json:"timestamp"`
 }
 
 func (u *Usecase) ProcessA15(ctx context.Context) error {
@@ -31,34 +30,54 @@ func (u *Usecase) ProcessA15(ctx context.Context) error {
 		return err
 	}
 
-	for i, wo := range wol {
-		isA15 := slices.ContainsFunc(wo.Devices, func(d entity.Device) bool {
-			return d.Type == string(entity.DeviceTypeA15)
-		})
+	a15Device := make([]entity.Device, 0)
 
-		if !isA15 {
-			continue
+	for _, wo := range wol {
+		for _, device := range wo.Devices {
+			if device.Type == string(entity.DeviceTypeA15) {
+				a15Device = append(a15Device, device)
+			}
 		}
+	}
+	a15Device = removeDuplicates(a15Device)
 
-
-
-
-
-
-
-
+	lrs := make([]A15Result, 0)
+	for _, device := range a15Device {
+		lr, err := connectToSamba(device)
+		if err != nil {
+			return err
+		}
+		lrs = append(lrs, lr...)
 	}
 
+	for _, lr := range lrs {
+		speciment, err := u.SpecimenRepository.FindByBarcode(ctx, lr.PatientID)
+		if err != nil {
+			return err
+		}
 
+		observation := entity.ObservationResult{
+			SpecimenID:  int64(speciment.ID),
+			TestCode:    lr.TestName,
+			Description: lr.TestName,
+			Values:      []string{fmt.Sprintf("%.2f", lr.Value)},
+			Unit:        lr.Unit,
+			Date:        lr.Timestamp,
+		}
 
+		err = u.ObservationResultRepository.Create(ctx, &observation)
+		if err != nil {
+			return err
+		}
+	}
 
+	return nil
 }
 
-func ConnectToSamba(device entity.Device) error {
-
+func connectToSamba(device entity.Device) ([]A15Result, error) {
 	conn, err := net.Dial("tcp", net.JoinHostPort(device.IPAddress, strconv.Itoa(device.Port)))
 	if err != nil {
-		return err
+		return nil, err
 	}
 	defer conn.Close()
 
@@ -71,44 +90,43 @@ func ConnectToSamba(device entity.Device) error {
 
 	s, err := d.Dial(conn)
 	if err != nil {
-		return err
+		return nil, err
 	}
 	defer s.Logoff()
 
 	fs, err := s.Mount(device.Type)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
 	defer fs.Umount()
 
 	fil, err := fs.ReadDir(".")
 	if err != nil {
-		return err
+		return nil, err
 	}
-
+	lrs := make([]A15Result, 0)
 	for _, fi := range fil {
 		f, err := fs.Open(fi.Name())
 		if err != nil {
-			slog.Error(err)
+			return nil, err
 		}
 
-		b := io.ReadAll(f)
+		b, _ := io.ReadAll(f)
 		lr, err := ParseLabResults(string(b))
 		if err != nil {
-			return err
+			return nil, err
 		}
-
+		lrs = append(lrs, lr...)
 	}
-
-
+	return lrs, nil
 }
 
 // String provides a string representation of LabResult, useful for printing.
 func (lr A15Result) String() string {
 	return fmt.Sprintf(
 		"Patient: %s, Test: %s, Sample: %s, Value: %.3f %s, Time: %s",
-		lr.PatientName,
+		lr.PatientID,
 		lr.TestName,
 		lr.SampleType,
 		lr.Value,
@@ -158,14 +176,28 @@ func ParseLabResults(data string) ([]A15Result, error) {
 		}
 
 		results = append(results, A15Result{
-			PatientName: patientID,
-			TestName:    testName,
-			SampleType:  sampleType,
-			Value:       value,
-			Unit:        unit,
-			Timestamp:   timestamp,
+			PatientID:  patientID,
+			TestName:   testName,
+			SampleType: sampleType,
+			Value:      value,
+			Unit:       unit,
+			Timestamp:  timestamp,
 		})
 	}
 
 	return results, nil
+}
+
+func removeDuplicates(slice []entity.Device) []entity.Device {
+	seen := make(map[int]struct{})
+	var result []entity.Device
+
+	for _, item := range slice {
+		if _, exists := seen[item.ID]; !exists { // Assuming `ID` is a unique identifier for `entity.Device`
+			seen[item.ID] = struct{}{}
+			result = append(result, item)
+		}
+	}
+
+	return result
 }
