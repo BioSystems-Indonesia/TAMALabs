602b019628772155d6ef2d0873cf3126035119e3
dafaath <78836795+dafaath@users.noreply.github.com>
2025-07-16 06:52:56 +0700

    feat(migration): Remove non-unique rows before adding unique constraint to units (#96)

--- migrations/20250715233918_add_unique_constraint_units.up.sql
+++ migrations/20250715233918_add_unique_constraint_units.up.sql
@@ -1,2 +1,10 @@
+-- Remove non-unique rows before creating the unique index
+DELETE FROM `units`
+WHERE rowid NOT IN (
+  SELECT MIN(rowid)
+  FROM `units`
+  GROUP BY `base`, `value`
+);
+
 -- create index "units_base_value_uniq" to table: "units"
 CREATE UNIQUE INDEX `units_base_value_uniq` ON `units` (`base`, `value`);
